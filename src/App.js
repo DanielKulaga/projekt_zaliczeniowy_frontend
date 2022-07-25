15df62a1b7a3435c728d42734312f835efc3e35d
DanielKulaga <danielkulaga@outlook.com>
2022-07-26 00:07:37 +0200

    create context to application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Header from "./components/header/Header"
+import MenuList from "./components/MenuList/MenuList"
+import {ShoppingCardContextProvider} from "./context/shoppingCardContext";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <ShoppingCardContextProvider>
+          <Router>
+              <Header/>
+              <Routes>
+                  <Route path="/menuList" element={<MenuList/>}/>
+                  <Route path="/" element={<MenuList/>}/>
+              </Routes>
+          </Router>
+      </ShoppingCardContextProvider>
   );
 }
 
