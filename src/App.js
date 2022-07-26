50089703b98073bce8cdf33d8727f579a8a80bb1
DanielKulaga <danielkulaga@outlook.com>
2022-07-26 23:56:44 +0200

    add new routes to shopping card and shipping address

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./components/header/Header"
 import MenuList from "./components/MenuList/MenuList"
 import {ShoppingCardContextProvider} from "./context/shoppingCardContext";
+import ShoppingCard from "./components/ShoppingCard/ShoppingCard";
+import Shipping from "./components/Shipping/Shipping";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
               <Header/>
               <Routes>
                   <Route path="/menuList" element={<MenuList/>}/>
+                  <Route path="/shopping-card" element={<ShoppingCard/>}/>
+                  <Route path="/shipping-address" element={<Shipping/>}/>
                   <Route path="/" element={<MenuList/>}/>
               </Routes>
           </Router>
