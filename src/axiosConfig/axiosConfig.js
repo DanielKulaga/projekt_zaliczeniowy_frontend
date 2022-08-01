7f1d5c3e7b09fe1c2fa0ad9cd991635f2ba26f7c
DanielKulaga <danielkulaga@outlook.com>
2022-08-01 22:08:02 +0200

    add new host for azure

--- src/axiosConfig/axiosConfig.js
+++ src/axiosConfig/axiosConfig.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
+
+function baseURL() {
+    if (window.location.hostname === 'localhost'){
+        return 'http://localhost:1323/';
+    } else {
+        return 'https://restaurant-ruczaj-server.azurewebsites.net';
+    }
+}
+
 export default axios.create({
-    baseURL: "http://localhost:1323",
+    baseURL: baseURL(),
 
     headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:1323, https://the-gadget-zone-server.azurewebsites.net',
+        'Access-Control-Allow-Origin': 'http://localhost:1323, https://restaurant-ruczaj-server.azurewebsites.net',
         'Content-Type': 'application/json',
         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     },
