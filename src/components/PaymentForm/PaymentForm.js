1fd1868a609d6f2f28536461e283f6c6ca920941
DanielKulaga <danielkulaga@outlook.com>
2022-08-01 23:42:25 +0200

    add azure address for payment success page

--- src/components/PaymentForm/PaymentForm.js
+++ src/components/PaymentForm/PaymentForm.js
@@ -58,7 +58,7 @@ export default function PaymentForm() {
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: "http://localhost:3000/payment-success",
+                return_url: "https://restaurant-ruczaj.azurewebsites.net/payment-success",
             },
         });
 
